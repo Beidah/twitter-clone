c83a7ad982a08b9516de178ae367f5979a93759c
Hunter Marshall <beidah@gmail.com>
2023-05-11 18:00:59 -0500

    Implemented new tweet backend

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,7 +1,8 @@
 import { useSession } from "next-auth/react";
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import { api } from "~/utils/api";
 
 function updateTextAreaHeight(textArea?: HTMLTextAreaElement) {
   if (textArea == null) return;
@@ -27,11 +28,25 @@ function Form() {
 
   useLayoutEffect(() => {
     updateTextAreaHeight(textAreaRef.current);
-  }, [inputValue])
+  }, [inputValue]);
+
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      console.log(newTweet);
+      setInputValue("");
+    }
+  });
 
   if (session.status !== "authenticated") return null;
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    createTweet.mutate({ content: inputValue });
+  }
+
   return (
-    <form className="flex flex-col gap-2 border-b px-4 py-2">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
         <ProfileImage src={session.data.user.image} />
         <textarea
