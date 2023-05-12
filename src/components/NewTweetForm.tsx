cfca0a4bbeb1de055553f13f744070bb913dbc9f
Hunter Marshall <beidah@gmail.com>
2023-05-11 20:48:11 -0500

    Update cache on new tweet

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -7,14 +7,14 @@ import { api } from "~/utils/api";
 function updateTextAreaHeight(textArea?: HTMLTextAreaElement) {
   if (textArea == null) return;
   textArea.style.height = "0";
-  textArea.style.height = `${textArea.scrollHeight}px`
+  textArea.style.height = `${textArea.scrollHeight}px`;
 }
 
 export default function NewTweetForm() {
   const session = useSession();
   if (session.status !== "authenticated") return null;
 
-  return <Form />
+  return <Form />;
 }
 
 function Form() {
@@ -25,6 +25,7 @@ function Form() {
     updateTextAreaHeight(textArea);
     textAreaRef.current = textArea;
   }, []);
+  const trpcUtils = api.useContext();
 
   useLayoutEffect(() => {
     updateTextAreaHeight(textAreaRef.current);
@@ -32,8 +33,35 @@ function Form() {
 
   const createTweet = api.tweet.create.useMutation({
     onSuccess: (newTweet) => {
-      console.log(newTweet);
       setInputValue("");
+
+      if (session.status !== "authenticated") return;
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return;
+
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name || null,
+            image: session.data.user.image || null,
+          }
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets]
+            },
+            ...oldData.pages.slice(1)
+          ]
+        };
+      });
     }
   });
 
@@ -46,19 +74,19 @@ function Form() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
+    <form onSubmit={ handleSubmit } className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        <ProfileImage src={session.data.user.image} />
+        <ProfileImage src={ session.data.user.image } />
         <textarea
-          ref={inputRef}
-          style={{ height: 0 }}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          ref={ inputRef }
+          style={ { height: 0 } }
+          value={ inputValue }
+          onChange={ (e) => setInputValue(e.target.value) }
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?"
         />
       </div>
       <Button className="self-end">Tweet</Button>
     </form>
-  )
+  );
 }
